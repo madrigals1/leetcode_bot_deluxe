b130d151334c0dc552dce775999aa7540cf8ef0d
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-07-19 14:31:03 +0200

    Add BACKEND_JWT_REFRESH_TOKEN

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,14 @@
 import "dotenv/config";
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+export const BACKEND_JWT_REFRESH_TOKEN =
+  process.env.BACKEND_JWT_REFRESH_TOKEN;
 
 if (!TELEGRAM_BOT_TOKEN) {
   console.error("Error: TELEGRAM_BOT_TOKEN is not set.");
+  process.exit(1);
+}
+
+if (!BACKEND_JWT_REFRESH_TOKEN) {
+  console.error("Error: BACKEND_JWT_REFRESH_TOKEN is not set.");
   process.exit(1);
 }
