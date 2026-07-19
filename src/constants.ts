e21f9f51c949214044f2e3eb50a3058d4837320a
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-07-19 10:16:37 +0200

    Add telegram bot

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+if (!TELEGRAM_BOT_TOKEN) {
+  console.error("Error: TELEGRAM_BOT_TOKEN is not set.");
+  process.exit(1);
+}
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Bot } from "grammy";
+import { TELEGRAM_BOT_TOKEN } from "./constants";
+
+const bot = new Bot(TELEGRAM_BOT_TOKEN!);
+
+bot.command("start", (ctx) =>
+  ctx.reply("Welcome! I am the LeetCode Bot Deluxe.")
+);
+
+bot.on("message", (ctx) =>
+  ctx.reply("I only understand /start for now.")
+);
+
+bot.start();
