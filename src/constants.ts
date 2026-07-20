793fd05a890e4e87c48f35a177e89de4789ba645
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-07-20 22:18:14 +0200

    Add BACKEND_URL + function to fetch required env variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,16 @@
 import "dotenv/config";
-export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-export const BACKEND_JWT_REFRESH_TOKEN =
-  process.env.BACKEND_JWT_REFRESH_TOKEN;
 
-if (!TELEGRAM_BOT_TOKEN) {
-  console.error("Error: TELEGRAM_BOT_TOKEN is not set.");
-  process.exit(1);
+function getRequiredEnv(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    console.error(`Error: ${key} is not set.`);
+    process.exit(1);
+  }
+  return value;
 }
 
-if (!BACKEND_JWT_REFRESH_TOKEN) {
-  console.error("Error: BACKEND_JWT_REFRESH_TOKEN is not set.");
-  process.exit(1);
-}
+export const TELEGRAM_BOT_TOKEN = getRequiredEnv("TELEGRAM_BOT_TOKEN");
+export const BACKEND_JWT_REFRESH_TOKEN = getRequiredEnv(
+  "BACKEND_JWT_REFRESH_TOKEN",
+);
+export const BACKEND_URL = getRequiredEnv("BACKEND_URL");
