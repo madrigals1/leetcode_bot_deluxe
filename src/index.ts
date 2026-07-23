736b1c830609574f1d8957975b6bbcdcaebb2b2b
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-07-24 00:23:40 +0200

    Move commands.ts to command folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Bot } from "grammy";
 import { TELEGRAM_BOT_TOKEN } from "./constants";
-import { registerCommands } from "./commands";
+import { registerCommands } from "./command";
 
 const bot = new Bot(TELEGRAM_BOT_TOKEN!);
 
