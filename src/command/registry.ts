a36d2298ebab2f563fe04fe0491d6c9dabb77faf
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2026-08-10 20:35:40 +0200

    Remove unnecessary log

--- src/command/registry.ts
+++ src/command/registry.ts
@@ -41,8 +41,6 @@ export class CommandRegistry {
       try {
         await metadata.handler(ctx);
       } catch (error) {
-        console.log(error);
-
         if (error instanceof LeetCodeBotError) {
           await ctx.reply(error.message);
           return;
